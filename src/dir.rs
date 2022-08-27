ac6bbeb20d006924fe03c104294adeb2c6594363
Keifer Joedicker <kjoedicker@merkleinc.com>
2022-08-27 13:39:06 -0400

    feat(dir): implement and make `find_project_filepaths` a public method

--- src/dir.rs
+++ src/dir.rs
@@ -1,11 +1,28 @@
-// MOCK
-pub fn aggregrate_files() -> Vec<String>{
-    // TODO: align this with reality
-    let mut filepaths = vec![];
+use std::process::Command;
 
-    for i in 1..51 {
-        filepaths.push(format!("./test-todos/todo{i}.rs"));
-    }
+pub fn find_project_filepaths() -> Vec<String> {
+    // TODO(#0): make this project agnostic
+    let file_type = "*.rs";
 
+    // We leverage `find` because thats a lot easier
+    // than trying to reinvent the wheel. We can swallow
+    // the cost a shell call
+    let output =
+        (Command::new("find")
+                .arg(".")
+                .arg("-name")
+                .arg(file_type)
+                .output()
+                .expect("failed to execute process"))
+                .stdout;
+
+    let stringified_output = String::from_utf8(output).unwrap();
+
+    let filepaths: Vec<String> = stringified_output
+        .split("\n")
+        .filter(|x| x.len() != 0)
+        .map(|x| x.to_string())
+        .collect();
+    
     filepaths
 }
