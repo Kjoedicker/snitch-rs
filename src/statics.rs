23aeba708e683e60067f9b0cc89a3cc5bcc206be
kjoedicker <kjoedicker@merkleinc.com>
2022-08-28 16:47:24 -0400

    feat(static): make configuration loading a `lazy_static` step

--- src/statics.rs
+++ src/statics.rs
@@ -1,7 +1,12 @@
 use lazy_static::lazy_static;
 use regex::{ Regex };
 
+use crate::config::*;
+
 lazy_static! {
     pub static ref UNTAGGED_TODO_PATTERN: Regex = Regex::new(r"^(.*)TODO: (.*)").unwrap();
     pub static ref COMPLETED_TODO_PATTERN: Regex = Regex::new(r"DONE\(#(\d+)\):").unwrap();
+
+    #[derive(Debug)]
+    pub static ref CONFIG: Config = init();
 }
