1e78fe03294e7ff94d4a3f16ad2b925e35d77673
Kjoedicker <kjoedicker@gmail.com>
2023-03-11 18:20:07 -0500

    feat(commands/commit, commands/mod): add commit specific functionality into its own module

---
+++ src/commands/commit.rs
@@ -0,0 +1,103 @@
+use std::{
+    process::{ Command },
+};
+
+pub fn format_issues(issues: Vec<String>) -> String {
+
+    let concated_issues = format!(
+        "#{}", 
+        issues.join(", #")
+    );
+
+    concated_issues
+}
+
+pub fn format_commit_message(issues: &String) -> String {
+
+    let base_message = format!(
+        "Adding {}", 
+        match issues.len() > 1 {
+            true => "issues: ",
+            _ => "issue: "
+        }
+    );
+
+    let commit_message = format!(
+        "{}{}",
+        base_message, 
+        issues
+    );
+
+    commit_message
+}
+
+fn commit_file(filepath: &str, commit_message: String) {
+
+    Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(&commit_message)
+        .arg("--include")
+        .arg(filepath)
+        .output()
+        .expect(
+            &format!(
+                "Failed to commit '{}'\n for: {}`", 
+                commit_message,
+                filepath 
+            )
+        ).stdout;
+}
+
+pub fn commit_reported_issues(filepath: &str, issues: Vec<String>) {
+
+    let formatted_issues = format_issues(issues);
+    let commit_message= format_commit_message(&formatted_issues);
+
+    commit_file(&filepath, commit_message);
+
+    println!("[COMMITTED] issues: {}", formatted_issues);
+}
+
+#[cfg(test)]
+mod commit_tests {
+    use super::*;
+
+    fn str_to_string(val: &str) -> String {
+        String::from(val)
+    }
+
+    #[test]
+    fn test_format_issues() {
+        let test_issues: Vec<String> = vec![
+            str_to_string("1"),
+            str_to_string("2"),
+            str_to_string("3")
+        ];
+
+        let formatted_issues = format_issues(test_issues);
+
+        let expectation = true;
+        let reality = formatted_issues == "#1, #2, #3";
+
+        assert_eq!(expectation, reality, "Issues should be formatted properly");
+    }
+
+    #[test]
+    fn test_format_commit_message() {
+        let test_issues: Vec<String> = vec![
+            str_to_string("1"),
+            str_to_string("2"),
+            str_to_string("3")
+        ];
+
+        let formatted_issues = format_issues(test_issues);
+
+        let commit_message = format_commit_message(&formatted_issues);
+
+        let expectation = true;
+        let reality = commit_message == "Adding issues: #1, #2, #3";
+
+        assert_eq!(expectation, reality);
+    }
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,7 @@
+mod commit;
 mod peek;
 mod snitch;
 
+pub use commit::*;
 pub use peek::peek;
 pub use snitch::snitch;
