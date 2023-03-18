2f2fd7560fe8236ca4c0412583b5422669524b90
kjoedicker <kjoedicker@merkleinc.com>
2023-03-18 08:17:42 -0400

    refactor(src/helpers, src/trackers/github): replace `build_string_query` -> `build_query_string`

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,7 @@
 pub type QueryParam<'a> = (&'a str, &'a str);
 pub type QueryParams<'a> = Vec<QueryParam<'a>>;
 
-pub fn build_string_query(query: QueryParams) -> String {
+pub fn build_query_string(query: QueryParams) -> String {
     query.iter().fold(String::new(), |acc, &tuple| {
         acc + tuple.0 + "=" + tuple.1 + "&"
     })
