0fd0c0131b608a53fe164bd32a38f46253995337
Kjoedicker <kjoedicker@gmail.com>
2023-04-01 19:59:14 -0400

    feat(trackers/mod, trackers/tracker): add a base tracker `Issue` type and `IssueTracker` trait that new trackers can implement

---
+++ src/trackers/tracker.rs
@@ -0,0 +1,16 @@
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Deserialize, Debug, Serialize)]
+pub struct Issue {
+    pub html_url: String,
+    pub title: String,
+    pub number: u32,
+}
+
+#[async_trait]
+pub trait IssueTracker {
+    fn build_request_url(&self) -> String;
+    async fn fetch_issues(&self) -> Vec<Issue>;
+    async fn create_issue(&self, title: &str) -> Issue;
+}
