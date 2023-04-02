6e330bf7833613563f17271d1f1a2b8547498df7
Kjoedicker <kjoedicker@gmail.com>
2023-04-02 08:56:43 -0400

    refactor(statics, commands/peek, commands/snitch): remove dependency on global `CONFIG`

--- src/statics.rs
+++ src/statics.rs
@@ -1,13 +1,8 @@
 use lazy_static::lazy_static;
 use regex::{ Regex };
 
-use crate::config::*;
-
 lazy_static! {
-    //TODO: make this pattern configurable and more dynamic
+    // TODO: make matching patterns configurable
     pub static ref UNTAGGED_ISSUE_PATTERN: Regex = Regex::new(r"^(.*)TODO: (.*)").unwrap();
     pub static ref COMPLETED_ISSUE_PATTERN: Regex = Regex::new(r"DONE\(#(\d+)\):").unwrap();
-
-    #[derive(Debug)]
-    pub static ref CONFIG: Config = init();
 }
