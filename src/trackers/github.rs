d9500fe7d81896291fea983da6841e171cc88234
Kjoedicker <kjoedicker@gmail.com>
2023-04-01 20:08:24 -0400

    feat(trackers/github): implement `Github` and `IssueTracker` for instances of `Github` // update testing around methods

--- src/trackers/github.rs
+++ src/trackers/github.rs
@@ -1,150 +1,194 @@
+use super::tracker::{IssueTracker, Issue};
+use crate::config::Config;
 use crate::{ 
     helpers::*,
     statics::*
 }; 
+use async_trait::async_trait;
 use reqwest::{Client, StatusCode };
 use reqwest::header::{USER_AGENT, AUTHORIZATION};
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 
-#[derive(Clone, Deserialize, Debug, Serialize)]
-pub struct Issue {
-    pub html_url: String,
-    pub title: String,
-    pub number: u32,
+pub struct Github {
+    base_tracker_url: String,
+    owner: String,
+    repo: String,
+    issues_per_request: String,
+    token: String
 }
 
-fn build_request_url() -> String {
-    format!("{}/repos/{}/{}/issues", CONFIG.base_tracker_url, CONFIG.owner, CONFIG.repo)
+pub fn init_instance(config: Config) -> Github {
+    Github {
+        base_tracker_url: config.base_tracker_url,
+        owner: config.owner,
+        issues_per_request: config.issues_per_request,
+        repo: config.repo,
+        token: config.token
+    }
 }
 
-pub async fn fetch_issues() -> Vec<Issue> {
-    let client = Client::new();
-    
-    let query_string = build_query_string(vec![
-        ("per_page", &CONFIG.issues_per_request)
-    ]);
-
-    let request_url = format!("{}?{}", build_request_url(), query_string);
-
-    let response = client
-        .get(request_url)
-        .header(USER_AGENT, "SnitchRs")
-        .header(AUTHORIZATION, &CONFIG.token)
-        .send()
-        .await
-        .unwrap();
-
-    match response.status() {
-        StatusCode::OK => {},
-        StatusCode::NOT_FOUND => {
-            panic!("Repo not found, check configuration");
-        },
-        StatusCode::UNAUTHORIZED => {
-            panic!("Request unauthorized, check access token");
-        }
-        status_code => {
-            panic!("Received error reaching out to github API: {:?}", status_code);
-        }
+#[async_trait]
+impl IssueTracker for Github {
+    fn build_request_url(&self) -> String {
+        format!("{}/repos/{}/{}/issues", self.base_tracker_url, self.owner, self.repo)
     }
 
-    let issues: Vec<Issue> = match response.json().await {
-        Ok(issues) => issues,
-        Err(err) => panic!("Problem marshaling response data into issue type, {:?}", err)
-    };
+    async fn fetch_issues(&self) -> Vec<Issue> {
+        let client = Client::new();
+        
+        let query_string = build_query_string(vec![
+            ("per_page", &self.issues_per_request)
+        ]);
 
-    issues
-}
+        let request_url = format!("{}?{}", self.build_request_url(), query_string);
 
-pub async fn create_issue(title: &str) -> Issue {
-    let client = Client::new();
+        let response = client
+            .get(request_url)
+            .header(USER_AGENT, "SnitchRs")
+            .header(AUTHORIZATION, &self.token)
+            .send()
+            .await
+            .unwrap();
 
-    let request_url = build_request_url();
+        match response.status() {
+            StatusCode::OK => {},
+            StatusCode::NOT_FOUND => {
+                panic!("Repo not found, check configuration");
+            },
+            StatusCode::UNAUTHORIZED => {
+                panic!("Request unauthorized, check access token");
+            }
+            status_code => {
+                panic!("Received error reaching out to github API: {:?}", status_code);
+            }
+        }
 
-    let request_body = json!({
-        "title": title,
-        "body": ""
-    });
+        let issues: Vec<Issue> = match response.json().await {
+            Ok(issues) => issues,
+            Err(err) => panic!("Problem marshaling response data into issue type, {:?}", err)
+        };
 
-    let response = client
-        .post(request_url)
-        .header(USER_AGENT, "SnitchRs")
-        .header(AUTHORIZATION, &CONFIG.token)
-        .json(&request_body)
-        .send()
-        .await
-        .unwrap();
+        issues
+    }
 
-    match response.status() {
-        StatusCode::CREATED => {},
-        StatusCode::OK => {},
-        status_code => panic!("Received error reaching out to github API: {:?}", status_code)
-    };
+    async fn create_issue(&self, title: &str) -> Issue {
+        let client = Client::new();
 
-    let issue: Issue = match response.json().await {
-        Ok(issue) => issue,
-        Err(err) => panic!("Problem marshaling response data into issue type, {:?}", err)
-    };
+        let request_url = self.build_request_url();
 
-    issue
+        let request_body = json!({
+            "title": title,
+            "body": ""
+        });
+
+        let response = client
+            .post(request_url)
+            .header(USER_AGENT, "SnitchRs")
+            .header(AUTHORIZATION, &CONFIG.token)
+            .json(&request_body)
+            .send()
+            .await
+            .unwrap();
+
+        match response.status() {
+            StatusCode::CREATED => {},
+            StatusCode::OK => {},
+            status_code => panic!("Received error reaching out to github API: {:?}", status_code)
+        };
+
+        let issue: Issue = match response.json().await {
+            Ok(issue) => issue,
+            Err(err) => panic!("Problem marshaling response data into issue type, {:?}", err)
+        };
+
+        issue
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::Value;
     use wiremock::{MockServer, Mock, ResponseTemplate};
     use wiremock::matchers::{method, path};
 
-    mod fetch_issues {
+    fn build_instance(base_tracker_url: String) -> Github {
+        Github { 
+            base_tracker_url: base_tracker_url, 
 
+            // default
+            repo: CONFIG.repo.clone(), 
+            owner: CONFIG.owner.clone(), 
+            issues_per_request: CONFIG.issues_per_request.clone(),
+            token: CONFIG.token.clone()
+        }
+    }
+
+    async fn build_mock_server(method_type: &str, status: StatusCode, json_body: Option<Value>) -> (MockServer, String) {
+        let mock_server = MockServer::start().await;
+
+        let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
+
+        let response = match json_body {
+            Some(data) => {
+                ResponseTemplate::new(status).set_body_json(data)
+            },
+            None => {
+                ResponseTemplate::new(status)
+            }
+        };
+
+        Mock::given(method(method_type))
+        .and(path(url_path.clone()))
+        .respond_with(response)
+        .mount(&mock_server)
+        .await;
+
+        let server_uri = mock_server.uri();
+    
+        (mock_server, server_uri)
+    }
+
+    mod fetch_issues {
         use super::*;
 
         #[tokio::test]
         #[should_panic(expected = "Repo not found")]
         async fn should_handle_404 (){
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("GET", StatusCode::NOT_FOUND, None).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
+            let github_tracker = build_instance(server_uri);
 
-            Mock::given(method("GET"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(404))
-            .mount(&mock_server)
-            .await;
-
-            let _ = fetch_issues(Some(mock_server.uri())).await;
+            let _ = github_tracker.fetch_issues().await;
         }
     
         #[tokio::test]
         #[should_panic(expected = "Request unauthorized, check access token")]
         async fn should_handle_401 (){
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("GET", StatusCode::UNAUTHORIZED, None).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
+            let github_tracker = build_instance(server_uri);
 
-            Mock::given(method("GET"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(401))
-            .mount(&mock_server)
-            .await;
-
-            let _ = fetch_issues(Some(mock_server.uri())).await;
+            let _ = github_tracker.fetch_issues().await;
         }
 
         #[tokio::test]
         #[should_panic(expected = "Received error reaching out to github API: 451")]
         async fn should_handle_unexpected_error (){
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("GET", StatusCode::UNAVAILABLE_FOR_LEGAL_REASONS, None).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
+            let github_tracker = build_instance(server_uri);
 
-            Mock::given(method("GET"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(451))
-            .mount(&mock_server)
-            .await;
-
-            let _ = fetch_issues(Some(mock_server.uri())).await;
+            let _ = github_tracker.fetch_issues().await;
         }
 
         #[tokio::test]
@@ -162,17 +206,14 @@ mod tests {
               },
             ]);
     
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("GET", StatusCode::OK, Some(json_body)).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
-        
-            Mock::given(method("GET"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(200).set_body_json(json_body))
-            .mount(&mock_server)
-            .await;
+            let github_tracker = build_instance(server_uri);
 
-            let issues = fetch_issues(Some(mock_server.uri())).await;
+            let issues = github_tracker.fetch_issues().await;
 
             assert_eq!(issues.len(), 2, "There should be a total of two issues");
 
@@ -187,17 +228,14 @@ mod tests {
 
             let json_body = serde_json::json!([{}]);
     
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("GET", StatusCode::OK, Some(json_body)).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
-        
-            Mock::given(method("GET"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(200).set_body_json(json_body))
-            .mount(&mock_server)
-            .await;
+            let github_tracker = build_instance(server_uri);
 
-            let _ = fetch_issues(Some(mock_server.uri())).await;
+            let _ = github_tracker.fetch_issues().await;
         }
     }
 
@@ -214,17 +252,14 @@ mod tests {
               }
             );
     
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("POST", StatusCode::OK, Some(json_body)).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
-        
-            Mock::given(method("POST"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(200).set_body_json(json_body))
-            .mount(&mock_server)
-            .await;
+            let github_tracker = build_instance(server_uri);
 
-            let issue = create_issue("test-title", Some(mock_server.uri())).await;
+            let issue = github_tracker.create_issue("test-title").await;
         
             assert_eq!(issue.html_url, "https://github.com/Kjoedicker/snitch-lab/issues/650");
             assert_eq!(issue.number, 650);
@@ -237,17 +272,14 @@ mod tests {
 
             let json_body = serde_json::json!({});
     
-            let mock_server = MockServer::start().await;
+            let (
+                _mock_server, 
+                server_uri
+            ) = build_mock_server("POST", StatusCode::OK, Some(json_body)).await;
 
-            let url_path = format!("/repos/{}/{}/issues", CONFIG.owner, CONFIG.repo);
-        
-            Mock::given(method("POST"))
-            .and(path(url_path))
-            .respond_with(ResponseTemplate::new(200).set_body_json(json_body))
-            .mount(&mock_server)
-            .await;
+            let github_tracker = build_instance(server_uri);
 
-            let _ = create_issue("test-title", Some(mock_server.uri())).await;
+            let _ = github_tracker.create_issue("test-title").await;
         }
     }
 }
